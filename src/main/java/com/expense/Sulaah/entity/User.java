b0f316cb86666a57701f3e41625a1acc3ed27e56
MASTREX <38161604+MASTREX@users.noreply.github.com>
2024-01-07 14:27:42 +0530

    entity mapped

--- src/main/java/com/expense/Sulaah/entity/User.java
+++ src/main/java/com/expense/Sulaah/entity/User.java
@@ -1,15 +1,24 @@
 package com.expense.Sulaah.entity;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
+@Data
 @Table(name = "all_users")
 public class User {
 	@Id
 	private int id;
 	private String username;
 	private String email;
+
+	@ManyToMany(cascade = CascadeType.ALL, mappedBy = "usersInGroup")
+	private List<Group> groups;
 
 }
