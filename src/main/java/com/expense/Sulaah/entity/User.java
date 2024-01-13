4fa0409cbc265dd37a9a5968befec633c59dd30f
Shubham <Shubham20015>
2024-01-13 13:25:53 +0530

    update entities

--- src/main/java/com/expense/Sulaah/entity/User.java
+++ src/main/java/com/expense/Sulaah/entity/User.java
@@ -3,10 +3,16 @@ package com.expense.Sulaah.entity;
 import java.util.List;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "users")
 public class User {
 	@Id
@@ -17,13 +23,13 @@ public class User {
 	private String username;
 	@Column(name = "email", unique = true)
 	private String email;
-	@Column(name = "debit_amount")
-	private double debitAmount = 0.0;
-	@Column(name = "credit_amount")
-	private double creditAmount = 0.0;
-	@ManyToMany(cascade = CascadeType.ALL, mappedBy = "usersInGroup")
-	// TODO: Remove following comment by adding knowledge to wiki
-	//	@JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+	@Column(name = "debit_amount", columnDefinition = "DOUBLE DEFAULT 0.0")
+	private double debitAmount;
+	@Column(name = "credit_amount", columnDefinition = "DOUBLE DEFAULT 0.0")
+	private double creditAmount;
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(name = "users_group_map", joinColumns = @JoinColumn(name = "user_id"),
+			inverseJoinColumns = @JoinColumn(name = "group_id"))
 	private List<Group> groups;
 
 }
